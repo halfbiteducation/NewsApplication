71f44741137f7c27145d1de016bce919e8aa2d1c
prakhar agarwal <halfbiteducation@gmail.com>
2018-05-17 10:26:43 +0530

    added share menu in detail activity

--- app/src/main/java/com/example/prakharagarwal/newsapplication/DetailsActivity.java
+++ app/src/main/java/com/example/prakharagarwal/newsapplication/DetailsActivity.java
@@ -1,8 +1,12 @@
 package com.example.prakharagarwal.newsapplication;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -14,6 +18,30 @@ public class DetailsActivity extends AppCompatActivity {
     TextView descView;
     ImageView imageView;
     TextView link;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu_detail,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id=item.getItemId();
+        if(id==R.id.menu_item_share){
+            Intent sendIntent = new Intent();
+            sendIntent.setAction(Intent.ACTION_SEND);
+            sendIntent.putExtra(Intent.EXTRA_SUBJECT, getIntent().getStringExtra("headline"));
+            sendIntent.putExtra(Intent.EXTRA_TEXT, getIntent().getStringExtra("url"));
+            sendIntent.setType("text/plain");
+//            startActivity(sendIntent);
+            startActivity(Intent.createChooser(sendIntent, "Share"));
+        }
+        return true;
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
